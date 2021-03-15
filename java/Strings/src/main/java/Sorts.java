a5dcd68c09b5e98961956d067d7bbdc8181c6953
William Hammond <william.t.hammond@gmail.com>
2021-03-14 20:48:53 -0400

    Adds Strings module scaffolding

---
+++ java/Strings/src/main/java/Sorts.java
@@ -0,0 +1,17 @@
+public class Sorts {
+    public static String keyIndexed(String text) {
+        return text;
+    }
+
+    public static String lsd(String text) {
+        return text;
+    }
+
+    public static String msd(String text) {
+        return text;
+    }
+
+    public static String quicksort(String text) {
+        return text;
+    }
+}
---
+++ java/Strings/src/main/java/Substring.java
@@ -0,0 +1,13 @@
+public class Substring {
+  public static int knuthMorricPratt(String text, String pattern) {
+    return 0;
+  }
+
+  public static int boyerMoore(String text, String pattern) {
+    return 0;
+  }
+
+  public static int fingerPrint(String text, String pattern) {
+    return 0;
+  }
+}
