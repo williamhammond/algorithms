bf02d3060393074745187120db5bfceb70802204
William Hammond <william.t.hammond@gmail.com>
2021-03-14 21:01:35 -0400

    Adds stubbed compression algorithms

--- java/Strings/src/main/java/Compression.java
+++ java/Strings/src/main/java/Compression.java
@@ -1,2 +1,8 @@
 public class Compression {
+  public static Byte[] huffman(String input) {
+    return new Byte[] {};
+  }
+  public static Byte[] lzw(String input) {
+    return new Byte[] {};
+  }
 }
