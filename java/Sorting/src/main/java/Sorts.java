5e0172a47d94c20492fd807723c881187e892eea
WilliamHammond <william.t.hammond@gmail.com>
2021-03-21 11:03:25 -0400

    Implements Selection Sort

--- java/Sorting/src/main/java/Sorts.java
+++ java/Sorting/src/main/java/Sorts.java
@@ -1,13 +1,37 @@
+/**
+ *
+ */
 public class Sorts {
-  public static <T extends Comparable<? super T>> void mergeSort(Comparable<T>[] a) {}
+    public static <T extends Comparable<? super T>> void mergeSort(T[] a) {
+    }
 
-  public static <T extends Comparable<? super T>> void selectionSort(Comparable<T>[] a) {}
+    public static <T extends Comparable<? super T>> void selectionSort(T[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j].compareTo(a[min]) < 0) {
+                    min = j;
+                }
+            }
+            swap(a, i, min);
+        }
+    }
 
-  public static <T extends Comparable<? super T>> void quickSort(Comparable<T>[] a) {}
+    private static <T extends Comparable<? super T>> void swap(T[] a, int i, int j) {
+        T temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 
-  public static <T extends Comparable<? super T>> void heapSort(Comparable<T>[] a) {}
+    public static <T extends Comparable<? super T>> void quickSort(T[] a) {
+    }
 
-  public static <T extends Comparable<? super T>> void countSort(Comparable<T>[] a) {}
+    public static <T extends Comparable<? super T>> void heapSort(T[] a) {
+    }
 
-  public static <T extends Comparable<? super T>> void bucketSort(Comparable<T>[] a) {}
+    public static <T extends Comparable<? super T>> void countSort(T[] a) {
+    }
+
+    public static <T extends Comparable<? super T>> void bucketSort(T[] a) {
+    }
 }
--- java/Sorting/src/test/java/TestSorts.java
+++ java/Sorting/src/test/java/TestSorts.java
@@ -10,31 +10,30 @@ import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 public class TestSorts {
-  private static final List<Consumer<Comparable[]>> FUNCTIONS =
-      List.of(
-          Sorts::mergeSort,
-          Sorts::selectionSort,
-          Sorts::quickSort,
-          Sorts::heapSort,
-          Sorts::countSort,
-          Sorts::bucketSort);
+    private static final List<Consumer<Comparable[]>> FUNCTIONS =
+            List.of(
+                    Sorts::selectionSort
+            );
 
-  @ParameterizedTest
-  @MethodSource("providesSimpleSort")
-  public void itSimpleSorts(
-      Consumer<Comparable[]> function, Comparable[] input, Comparable[] output) {
-    function.accept(input);
-    assertArrayEquals(output, input);
-  }
-
-  private static Stream<Arguments> providesSimpleSort() {
-    Stream.Builder<Arguments> streamBuilder = Stream.builder();
-    for (Consumer<Comparable[]> function : FUNCTIONS) {
-      streamBuilder.add(Arguments.of(function, new Integer[] {1, 2, 3}, new Integer[] {1, 2, 3}));
-      streamBuilder.add(
-          Arguments.of(function, new Character[] {'a', 'b', 'c'}, new Character[] {'a', 'b', 'c'}));
+    @ParameterizedTest
+    @MethodSource("providesSimpleSort")
+    public void itSimpleSorts(
+            Consumer<Comparable[]> function, Comparable[] input, Comparable[] output) {
+        function.accept(input);
+        assertArrayEquals(output, input);
     }
 
-    return streamBuilder.build();
-  }
+    private static Stream<Arguments> providesSimpleSort() {
+        Stream.Builder<Arguments> streamBuilder = Stream.builder();
+        for (Consumer<Comparable[]> function : FUNCTIONS) {
+            streamBuilder.add(Arguments.of(function, new Integer[]{1, 2, 3}, new Integer[]{1, 2, 3}));
+            streamBuilder.add(
+                    Arguments.of(function, new Character[]{'a', 'b', 'c'}, new Character[]{'a', 'b', 'c'}));
+            streamBuilder.add(Arguments.of(function, new Integer[]{3, 2, 1}, new Integer[]{1, 2, 3}));
+            streamBuilder.add(Arguments.of(function, new Integer[]{1, 1, 1}, new Integer[]{1, 1, 1}));
+            streamBuilder.add(Arguments.of(function, new Integer[]{0, 1, -1}, new Integer[]{-1, 0, 1}));
+        }
+
+        return streamBuilder.build();
+    }
 }
